3cf16b1afd34320b099fa97bd0d2b877e6016e6a
Carmen <Carmen@192.168.56.1>
2018-07-26 16:24:27 -0300

    prueba

---
+++ ObligatorioTaller/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <script src="Librerias/js/jquery-3.3.1.min.js" type="text/javascript"></script>
+        
+        <!-- Boostrap -->
+        <link href="Librerias/Bootstrap/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="Librerias/Bootstrap/bootstrap-3.3.7-dist/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
+        
+        <title></title>
+    </head>
+    <body>
+          <h1> Bienvenido Obligatorio</h1>
+          <p><a href="registroUsuario.php" target="_blank">Registro Usuario</a> </p>
+    </body>
+</html>
